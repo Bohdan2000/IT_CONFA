ece92c4c9729ad9347f504178880776d15e73fd2
Bohdan__Mayorchak <bohdan.ma@starkey.co.il>
2020-03-16 16:38:24 +0200

    [ADD] Added integration with back4app api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CountriesModule } from './modules/country/country.module';
 import { DatabaseModule } from './modules/database/database.module';
+import { Back4AppModule } from './modules/back4app/back4app.module';
 
 
 @Module({
-  imports: [DatabaseModule, CountriesModule],
+  imports: [DatabaseModule, CountriesModule, Back4AppModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/app.config.ts
@@ -0,0 +1,6 @@
+import { get } from 'config';
+
+export default {
+  PORT: get('port'),
+  BACK4APP_BASE_URL: get('back4app.base_url')
+};
