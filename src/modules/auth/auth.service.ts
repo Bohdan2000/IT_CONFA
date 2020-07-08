929ae2973c63460eefaeea92ca319ca0e55b3c35
Bohdan__Mayorchak <bohdan.ma@starkey.co.il>
2020-07-08 13:24:45 +0300

    Finished auth service.Added AuthGuard

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,31 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { SignInDto, SignUpDto } from './dto/auth.dto';
 import * as jwt from 'jsonwebtoken';
+import * as base64 from 'base-64';
 
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   async signIn(signInDto: SignInDto) {
     const { email, password } = signInDto;
     const user = await this.usersService.getUserByEmail(email);
-    if(!user) {
-      throw new Error('User not found by provided email');
+    if (!user) {
+      throw new HttpException('User not found by provided email', 404);
     }
-    if(user.password === password) {
-      const token = `Bearer ${jwt.sign(signInDto, 'secret')}`;
-      return {"token": token};
+    console.log('qwdqw', base64.decode(user.password) === password, base64.decode(user.password));
+    if (base64.decode(user.password) === password) {
+      const token = `${jwt.sign({
+        id: user.id,
+        role: user.role,
+        email: user.email
+      }, 'secret')}`;
+      return { "token": token };
     }
-    else 
-      throw new Error('Password incorect'); 
+    else
+      throw new HttpException('Password incorect', 400);
   }
 
   async signUp(signUpDto: SignUpDto) {
-    const { email } = signUpDto;
+    const { email, password } = signUpDto;
+    signUpDto.password = base64.encode(password);
     if (await this.usersService.getUserByEmail(email)) {
-      throw new Error('This email already in use')
+      throw new HttpException('This email already in use', 400)
     }
     return this.usersService.create(signUpDto);
   }
